a8d88dd00d13c04a62a1e06615ed5ce3ab810b59
Seyda1 <seydaybar@gmail.com>
2020-09-21 09:36:14 +0300

    Upload

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{//Şeyda Aybar
+    //bubbleSort
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter  your array size");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[size];
+            Console.WriteLine("please enter  your array's elements...");
+            for (int i = 0; i < size; i++)
+            {Console.WriteLine(i + ".element:");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            bubble(array);
+            Console.WriteLine("sorted ARRAY:..\n");
+            for (int i = 0; i < size; i++)
+            {
+                
+                Console.WriteLine(" {0}", array[i]);
+            }
+            Console.ReadLine();
+        }
+        public static void bubble(int[] array)
+        {
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                int flag = 0;
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        flag = 1;
+                    }
+
+                }
+                if (flag == 0)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
